d0c5411820a404f043d5ac2394ef514f9bc87ef9
Julio Cesar Martin <juliomatcom@users.noreply.github.com>
2017-02-09 15:41:00 +0100

    style (spaces)

--- index.js
+++ index.js
@@ -12,7 +12,7 @@
    * }, { deep: true });
    * // { a: 1, b: [3, 4] }
    */
-  global.objectMap = function objectMap(obj, fn, options) {
+  global.objectMap = function objectMap (obj, fn, options) {
     var obj = obj && typeof obj === 'object' ? obj : {};
     var keys = Object.keys(obj);
     var length = keys.length;
@@ -20,7 +20,7 @@
     var result = Array.isArray(obj) ? [] : {};
     var i;
 
-    for(i = 0; i < length; i++) {
+    for (i = 0; i < length; i++) {
       result[keys[i]] = options.deep && notEmpty(obj[keys[i]]) ?
         objectMap(obj[keys[i]], fn, options) :
         fn(obj[keys[i]], keys[i], obj);
@@ -28,7 +28,7 @@
     return result;
   }
 
-  function notEmpty(obj) {
+  function notEmpty (obj) {
     return obj && typeof obj === 'object' && Object.keys(obj).length;
   }
 
